b0e61a9434a10913e738a0d287664393b8c9e3ad
sagar mounika <sagarmouni5@gmail.com>
2021-02-17 00:21:07 +0530

    refactored

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 let randomNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-console.log(randomNumber);
 let message = document.querySelector('.message');
 let highscore = 0;
+
+// AGAIN EVENT
 document.querySelector('.again').addEventListener('click', () => {
   randomNumber = Math.trunc(Math.random() * 20 + 1);
   score = 20;
@@ -15,26 +16,27 @@ document.querySelector('.again').addEventListener('click', () => {
   document.querySelector('.number').textContent = '?';
 });
 
+// CHECK EVENT
 document.querySelector('.check').addEventListener('click', () => {
   let guessNumber = Number(document.querySelector('.guess').value);
   if (!guessNumber || guessNumber > 20) {
     message.textContent = '❌Enter a Valid Number ';
   } else if (guessNumber === randomNumber) {
     message.textContent = '🎉🎉You Guessed it right!!!';
-    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = randomNumber;
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guessNumber > randomNumber && score > 1) {
-    message.textContent = '📈 Too High!!';
+  } else if (guessNumber !== randomNumber && score > 1) {
+    message.textContent =
+      guessNumber > randomNumber ? '📈 Too High!!' : '📉 Too Low!!';
     score--;
     document.querySelector('.score').textContent = score;
-  } else if (guessNumber < randomNumber && score > 1) {
-    message.textContent = '📉 Too Low!!';
-    score--;
-    document.querySelector('.score').textContent = score;
+  }else{
+    message.textContent = '😐You Lost the game!!!';
+    document.querySelector('.score').textContent = 0;
   }
 });
